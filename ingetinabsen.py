a12188ff6420b810fa22f7b4a57f243a82309f36
nabielherdiana <156826310+nabielherdiana@users.noreply.github.com>
2025-02-17 19:33:41 +0700

    Update ingetinabsen.py

--- ingetinabsen.py
+++ ingetinabsen.py
@@ -25,7 +25,7 @@ application.add_handler(CommandHandler("start", start))
 # ✅ Jadwal kuliah
 # Jadwal kuliah berdasarkan gambar
 jadwal_kuliah = [
-    {"hari": "Minggu", "waktu": "19:48", "mata_kuliah": "Analisis Data Multivariat 2"},
+    {"hari": "Senin", "waktu": "19:49", "mata_kuliah": "Analisis Data Multivariat 2"},
     {"hari": "Senin", "waktu": "10:00", "mata_kuliah": "Analisis Data Kategori"},
     {"hari": "Senin", "waktu": "13:30", "mata_kuliah": "Analisis Data Multivariat 2"},
     {"hari": "Selasa", "waktu": "07:30", "mata_kuliah": "Analisis Data Kategori"},
